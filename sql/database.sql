a49b15f5ddf2383b761551c23985eec07646fa23
errom502 <79211993+errom502@users.noreply.github.com>
2022-11-05 20:10:29 +0300

    Update database.sql

--- sql/database.sql
+++ sql/database.sql
@@ -20,7 +20,7 @@ create table category(id_category int PRAMARY KEY, name_category text);
 
 --для продавца:
 create table product_table(id_product int PRIMARY KEY, id_seller varchar(100),  product_name text,product_category text, product_description text not null default '-'::text,
-                           product_image bytea not null default "C:\Users\Egore\GolandProjects\Market-Bot\default_image.jpg",
+                           product_image bytea not null default "https://github.com/io-qar/Market-Bot/blob/master/default_image.jpg?raw=true",
                            product_cost float4 not null default 0.0, product_availability int,FOREIGN KEY (id_seller) REFERENCES user_table(login),
                            FOREIGN KEY (product_category) REFERENCES category(name_category));
 
