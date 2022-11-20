90fee49ffbca31b4efa5780a08ab300577bef2b5
errom502 <79211993+errom502@users.noreply.github.com>
2022-11-20 13:16:16 +0300

    fix db create, addet triggers for category ads score(up,down)

--- sql/create_database.go
+++ sql/create_database.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
-	"io"
 	"os"
 )
 
@@ -33,33 +32,112 @@ func CreateDataBase() {
 		"SELECT EXISTS (SELECT FROM pg_database WHERE datname = 'market_bot')")
 	err := row.Scan(&baseExist)
 	models.CheckError(err)
-	fmt.Println(baseExist)
+	//fmt.Println(baseExist)
 	if baseExist == true {
 		var dbURL string = os.Getenv("URLMARKET")
 		Db, err = pgx.Connect(context.Background(), dbURL)
 		models.CheckError(err)
-		fmt.Println("Successfully connected to MARKET database!")
+		fmt.Println("database exist!\nSuccessfully connected to MARKET database!")
 		return
 	}
-	sqlString := ""
-	file, err := os.Open("./sql/database.sql")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	defer file.Close()
-	data := make([]byte, 64)
-	for {
-		n, err := file.Read(data)
-		if err == io.EOF { // если конец файла
-			break // выходим из цикла
-		}
-		sqlString += string(data[:n])
-	}
-	defer file.Close()
 	_, err = Db.Exec(context.Background(), "create database market_bot;")
-	_, err = Db.Exec(context.Background(), sqlString)
 	models.CheckError(err)
+	//
+	var dbURL string = os.Getenv("URLMARKET")
+	Db, err = pgx.Connect(context.Background(), dbURL)
+	//
+	//--остальное:
+	_, err = Db.Exec(context.Background(), "create table user_table(login varchar(100) PRIMARY KEY NOT NULL,password varchar(100) NOT NULL,role varchar(100) DEFAULT 'customer');")
+	models.CheckError(err)
+	//
+	_, err = Db.Exec(context.Background(), "create table category(id_category serial,name_category text NOT NULL PRIMARY KEY,number_of_product_ads int default 0);")
+	models.CheckError(err)
+	//
+	//--для продавца:
+	_, err = Db.Exec(context.Background(), "create table product_table(id_product serial PRIMARY KEY,id_seller varchar(100) REFERENCES user_table(login) NOT NULL,product_name text NOT NULL,product_category text REFERENCES category(name_category) NOT NULL,product_description text not null default '-'::text,product_image bytea not null default pg_read_binary_file('/imgs/default_image.jpg'),product_cost int not null default 0,product_availability int NOT NULL);")
+	models.CheckError(err)
+	//
+	// --для покупателя:
+	_, err = Db.Exec(context.Background(), "create table shopping_cart_table(id_cart int PRIMARY KEY,id_user varchar(100) REFERENCES user_table(login),id_product int REFERENCES product_table(id_product));")
+	models.CheckError(err)
+	//
+	// -- заполнение категорий товара:
+	_, err = Db.Exec(context.Background(), "insert into category(name_category) values ('Одежда и обувь');")
+	models.CheckError(err)
+	//
+	_, err = Db.Exec(context.Background(), "insert into category(name_category) values ('Аксессуары к одежде');")
+	models.CheckError(err)
+	//
+	_, err = Db.Exec(context.Background(), "insert into category(name_category) values ('Бытовая техника');")
+	models.CheckError(err)
+	//
+	_, err = Db.Exec(context.Background(), "insert into category(name_category) values ('Электроника');")
+	models.CheckError(err)
+	//
+	_, err = Db.Exec(context.Background(), "insert into category(name_category) values ('Детские товары');")
+	models.CheckError(err)
+	//
+	_, err = Db.Exec(context.Background(), "insert into category(name_category) values ('Товары для хобби');")
+	models.CheckError(err)
+	//
+	_, err = Db.Exec(context.Background(), "insert into category(name_category) values ('Товары для дома и сада');")
+	models.CheckError(err)
+	//
+	_, err = Db.Exec(context.Background(), "insert into category(name_category) values ('Бытовая химия');")
+	models.CheckError(err)
+	//
+	_, err = Db.Exec(context.Background(), "insert into category(name_category) values ('Косметика');")
+	models.CheckError(err)
+	//
+	_, err = Db.Exec(context.Background(), "insert into category(name_category) values ('Остальные категории');")
+	models.CheckError(err)
+	//
+	// -- триггеры и функции для таблицы category
+	_, err = Db.Exec(context.Background(), `
+	create or REPLACE function categoryUp()
+   		 returns trigger AS
+   		 $$
+	begin
+	update category
+	set  number_of_product_ads = number_of_product_ads + 1
+	where name_category = new.product_category;
+	return new;
+	end;
+	$$
+	language 'plpgsql';`)
+	models.CheckError(err)
+	//
+	_, err = Db.Exec(context.Background(), `
+	create or REPLACE function categoryDown()
+    returns trigger AS
+    $$
+	begin
+	update category
+	set  number_of_product_ads = number_of_product_ads - 1
+	where name_category = new.product_category;
+	return new;
+	end;
+	$$
+	language 'plpgsql';	
+	`)
+	models.CheckError(err)
+	//
+	_, err = Db.Exec(context.Background(), `
+	create trigger category_after_insert
+    	after insert on product_table
+    	for each row
+    	execute  procedure categoryUp();
+	`)
+	models.CheckError(err)
+	//
+	_, err = Db.Exec(context.Background(), `
+	create trigger category_after_delete
+    	after delete on product_table
+    	for each row
+    	execute  procedure categoryUp();
+	`)
+	models.CheckError(err)
+	//
 	fmt.Println("Successfully created tables")
 
 }
--- sql/database.sql
+++ sql/database.sql
@@ -1,20 +1,5 @@
 
 
-
-
-create or replace function downgrade_value(value int)
-	RETURNS integer
-	LANGUAGE plpgsql
-	STRICT
-AS $function$
-declare
-output int := 0;
-begin
-output := value - 1;
-RETURN (output);
-END;
-$function$;
-
 drop table  if exists user_table;
 drop table  if exists category;
 drop table  if exists product_table;
@@ -22,7 +7,7 @@ drop table  if exists shopping_cart_table;
 
 
 --остальное:
-create table user_table( login varchar(100) PRIMARY KEY NOT NULL,
+create table user_table(login varchar(100) PRIMARY KEY NOT NULL,
                          password varchar(100) NOT NULL,
                          role varchar(100) DEFAULT 'customer');
 
@@ -31,7 +16,7 @@ create table category(id_category serial,
                       number_of_product_ads int default 0);
 
 --для продавца:
-create table product_table(id_product int PRIMARY KEY,
+create table product_table(id_product serial PRIMARY KEY,
                            id_seller varchar(100) REFERENCES user_table(login) NOT NULL,
                            product_name text NOT NULL,
                            product_category text REFERENCES category(name_category) NOT NULL,
@@ -55,5 +40,43 @@ insert into category(name_category) values ('Товары для хобби');
 insert into category(name_category) values ('Товары для дома и сада');
 insert into category(name_category) values ('Бытовая химия');
 insert into category(name_category) values ('Косметика');
+insert into category(name_category) values ('Остальные категории');
+
+
+-- триггеры для таблицы category
+create or REPLACE function categoryUp()
+    returns trigger AS
+    $$
+begin
+update category
+set  number_of_product_ads = number_of_product_ads + 1
+where name_category = new.product_category;
+return new;
+end;
+$$
+language 'plpgsql';
+
+create or REPLACE function categoryDown()
+    returns trigger AS
+    $$
+begin
+update category
+set  number_of_product_ads = number_of_product_ads - 1
+where name_category = new.product_category;
+return new;
+end;
+$$
+language 'plpgsql';
+
+
+create trigger category_after_insert
+    after insert on product_table
+    for each row
+    execute  procedure categoryUp();
+
+create trigger category_after_delete
+    after delete on product_table
+    for each row
+    execute  procedure categoryUp();
 
 
